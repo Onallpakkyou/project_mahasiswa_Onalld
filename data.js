a314d160c941b79db2d015c5f6d7f84d245efa33
Onallpakkyou <ronallade07@gmail.com>
2025-10-15 18:02:53 +0800

    Add files via upload

---
+++ data.js
@@ -0,0 +1,39 @@
+const dataMahasiswa = [
+  {
+    NIM: "5520124001",
+    Nama: "ANTONIUS KAE DA SILVA",
+    JK: "Pria",
+    TempatLahir: "LARANTUKA",
+    TanggalLahir: "29 September 2003",
+    HP: "082145687745",
+    Email: "dasilvatony362@gmail.com",
+  },
+  {
+    NIM: "5520124002",
+    Nama: "YOSEP RONALDO SILI TOKAN",
+    JK: "Pria",
+    TempatLahir: "LITE",
+    TanggalLahir: "31 Januari 2006",
+    HP: "082267886240",
+    Email: "ronallade07@gmail.com",
+  },
+  {
+    NIM: "5520124003",
+    Nama: "MARIANUS B. W. BELEN",
+    JK: "Pria",
+    TempatLahir: "NUBAWOTEN",
+    TanggalLahir: "15 Agustus 2002",
+    HP: "081338060616",
+    Email: "janbhelens@gmail.com",
+  },
+  {
+    NIM: "5520124004",
+    Nama: "DOMINIKA ANGGUNWINATA KOTEN",
+    JK: "Wanita",
+    TempatLahir: "LARANTUKA",
+    TanggalLahir: "3 November 2005",
+    HP: "082325485527",
+    Email: "winatakoten@gmail.com",
+  },
+  // tambahkan data lainnya dari PDF di sini...
+];
